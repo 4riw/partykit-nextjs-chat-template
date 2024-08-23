4dd39e0e101c6640142b2fb5f4cc6711024f9ceb
4riw <ari.wib@gmail.com>
2024-08-23 14:26:04 +0700

    Add files via upload

--- party/utils/openai.ts
+++ party/utils/openai.ts
@@ -26,7 +26,7 @@ export async function getChatCompletionResponse(
   });
 
   const openaiResponse = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
+    model: "nousresearch/hermes-3-llama-3.1-405b",
     stream: true,
     messages: prompt,
   });
